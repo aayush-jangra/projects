dc55c04077357f7eac22e9d28a6187f43c42cd7a
aayush-jangra <jangra.aayush@gmail.com>
2024-06-11 20:38:33 +0530

    setup typeorm

--- Instaliter/api/src/user/entity/user.entity.ts
+++ Instaliter/api/src/user/entity/user.entity.ts
@@ -1,22 +1,26 @@
 import { Field, Int, ObjectType } from "@nestjs/graphql";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+@Entity()
 @ObjectType()
 export class User {
     @Field(() => Int)
+    @PrimaryGeneratedColumn()
     id: number;
 
     @Field()
+    @Column()
     firstName: string;
 
+    @Column({nullable: true})
     @Field({nullable: true})
     lastName?: string;
 
+    @Column()
     @Field()
     email: string;
 
+    @Column()
     @Field()
     username: string;
-
-    @Field(() => [String])
-    friends: string[];
 }
